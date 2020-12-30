d75478ca4371aeecd02cee035d93a9de9c3f125c
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2020-12-30 19:23:52 +0100

    built first function to add a value to the blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,2 +1,12 @@
-age = 23
-name = 'Felix'
+# defining our blockchain as an empty list
+blockchain = [[1]]
+
+
+def add_value():
+    blockchain.append([blockchain[-1], 5.3])
+    print(blockchain)
+
+
+add_value()
+add_value()
+add_value()
