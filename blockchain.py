7a8d50ec61c8f941f6b1c4f6be72d6793b072a46
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2020-12-30 19:55:35 +0100

    using fuction with arguments

--- blockchain.py
+++ blockchain.py
@@ -2,11 +2,16 @@
 blockchain = [[1]]
 
 
-def add_value():
-    blockchain.append([blockchain[-1], 5.3])
+
+# writing a function to add values to the blockchain
+def add_value(transaction_amount):
+    blockchain.append([blockchain[-1], transaction_amount])
     print(blockchain)
 
 
-add_value()
-add_value()
-add_value()
+
+# invoke function
+add_value(2)
+add_value(0.9)
+add_value(10.89)
+
