763ac1d630b0ad489898f52fc9a14e408883e74e
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2020-12-30 20:28:09 +0100

    learning about the built-in input function

--- blockchain.py
+++ blockchain.py
@@ -11,11 +11,20 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
+# refactoring
+def get_user_input():
+    return float(input('Your transaction amount please: '))
 
-# invoke function
-add_value(2)
-add_value(0.9, get_last_blockchain_value())
-add_value(10.89, get_last_blockchain_value())
+
+# invoke function with repetitive use of built-in input function
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
 
 
 print(blockchain)
