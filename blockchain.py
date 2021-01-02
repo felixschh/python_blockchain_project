f5dae59394a27bb58252ca371d2c4f2d0515942d
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2021-01-02 16:31:02 +0100

    using range function to verify the chain

--- blockchain.py
+++ blockchain.py
@@ -34,25 +34,27 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('_' * 20)
 
 # a function used to validate if the blockchain is manipulated
 def verify_chain():
     block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        if block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
+            print_blockchain_elements()
             is_valid = False
-            break 
-        block_index += 1
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     """ Printing out the different input options """
     print('Please choose:')
     print('1: Add a new transacion value')
@@ -71,12 +73,11 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
         print('Invalid blockchain!')
         break
-
-
-print('Done!')
+else:
+    print('User left!')
