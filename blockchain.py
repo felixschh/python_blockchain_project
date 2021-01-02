bd43ba9bbf8259f0ebf70f1d566259222d3acbb1
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2021-01-02 14:43:50 +0100

    gerneral code improvements

--- blockchain.py
+++ blockchain.py
@@ -4,17 +4,21 @@ blockchain = []
 
 # defining function that returns last blockchain value
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
 # writing a function to add values to the blockchain
-def add_value(transaction_amount, last_transaction=[1]):
-    """ Append a new value as well as the last blockchainvalue to the blockchain """
-    blockchain.append([last_transaction, transaction_amount])
-    """Arguments:
-    :transation_amount: The amount should be added.
+def add_transaction(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchainvalue to the blockchain
+    Arguments:
+    :transaction_amount: The amount should be added.
     :last_transaction: The last blockchain transaction (default[1]).
     """
+    if last_transaction == None:
+        last_transaction = [1]
+    blockchain.append([last_transaction, transaction_amount])
 
 
 # function to get the users input of the amount of transaction
@@ -32,12 +36,6 @@ def print_blockchain_elements():
         print(block)
 
 
-
-# invoke get_user_input function to add a first value to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
-
 while True:
     """ Printing out the different input options """
     print('Please choose:')
@@ -49,7 +47,7 @@ while True:
     """ unsing conditions to invoke functions depending on users input """
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'q':
