a247c7d82155649bd3237a853e7284b2a0737feb
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2021-01-10 13:03:43 +0100

    use reduce and lambda fucntion

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,4 @@
+import functools
 # defining our blockchain as an empty list
 MINING_REWARD = 10
 
@@ -20,15 +21,17 @@ def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transaction'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount']for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
+    # amount_sent = 0
+    # for tx in tx_sender:
+    #     if len(tx) > 0:
+    #         amount_sent += tx[0]
     tx_recipient = [[tx['amount'] for tx in block['transaction'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
+    #amount_received = 0
+    # for tx in tx_recipient:
+    #     if len(tx) > 0:
+    #         amount_received += tx[0]
     return amount_received - amount_sent
 
 
@@ -170,6 +173,6 @@ while waiting_for_input:
     if not verify_chain():
         print('Invalid blockchain!')
         break
-    print('Balance of {}: {:6.2f}'.format('Max', get_balance('Max')))
+    print('Balance of {}: {:6.2f}'.format(owner, get_balance(owner)))
 else:
     print('User left!')
