8e062d788a599e597d6bd2cad580a5146a68325b
Felix Schekerka <felixschekerka@Felixs-MBP.fritz.box>
2021-01-22 11:46:22 +0100

    using init dundermethod for the first time

--- examples/oop/car.py
+++ examples/oop/car.py
@@ -1,6 +1,9 @@
 class Car:
-    top_speed = 100
-    warnings = []
+    # top_speed = 100
+    # warnings = []
+    def __init__(self, starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.warnings = []
 
 
     def drive(self):
@@ -8,9 +11,11 @@ class Car:
 
 car1 = Car()
 car1.drive()
+
+
 # Car.top_speed = 200
 car1.warnings.append('New warning')
-
+print(car1.warnings)
 
 car2 = Car()
 car2.drive()
